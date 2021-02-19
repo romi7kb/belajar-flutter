3b586d2244e611c186a8d602a98516aa64e78b30
romi7kb <romiramdhani84@gmial.com>
2021-02-19 09:15:13 +0700

    first commit

---
+++ lib/data-list.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+List<String> teman = [
+  'Bambang',
+  'Ketan',
+  'Flempoke',
+  'Lucyxfer',
+  'Shiro',
+  'Beehive',
+  'Nano'
+];
+List<Color> colorCodes = [
+  Colors.green,
+  Colors.amber,
+  Colors.blue,
+  Colors.indigo,
+  Colors.red,
+  Colors.yellow,
+  Colors.pinkAccent,
+];
